639d14c2af1ed17f83d2ff9ba74fb10ce073f1b1
RikuTerayama <rikuriku619center@gmail.com>
2025-10-03 03:58:00 +0900

    Delete js/perf-flags.js

--- js/perf-flags.js
+++
@@ -1,6 +0,0 @@
-// /js/perf-flags.js
-// Feature flags for safe perf tweaks. All default to true.
-// Toggle to false if any regression is observed.
-window.__PERF_FLAGS = {
-  passiveListeners: true   // Step 1 only: passive event listeners
-};
